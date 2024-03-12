8e0497a58c2f17d695565d37f46afe06de6551fa
ringsfrings <bilaltoaba@gmail.com>
2024-03-11 18:59:06 -0700

    changes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const firstline = 'C is fun\n';
-const secondline = 'Python is cool\n';
-const thirdline = 'JavaScript is amazing\n';
-console.log(firstline);
-console.log(secondline);
-console.log(thirdline);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
